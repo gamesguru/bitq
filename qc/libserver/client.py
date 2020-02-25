6e7866ee9233f965779ecd66ecb988393d6066ab
gamesguru <mathmuncher11@gmail.com>
2020-02-24 22:00:34 -0500

    update wips

--- qc/libserver/client.py
+++ qc/libserver/client.py
@@ -2,7 +2,8 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("69.14.146.157", 8333))
-client.send(b"I am CLIENT<br>")
+data = open('wolfram-alpha-1.4.1.apk', 'rb').read()
+client.send(data)
 from_server = client.recv(4096)
 client.close()
 print(from_server.decode())
--- qc/libserver/server.py
+++ qc/libserver/server.py
@@ -6,13 +6,14 @@ serv.bind(("0.0.0.0", 8333))
 serv.listen(5)
 while True:
     conn, addr = serv.accept()
-    from_client = ""
+    from_client = bytes()
+    conn.send(b"[Server] begin transfer")
     while True:
         data = conn.recv(4096)
         if not data:
             break
-        from_client += data.decode()
-        print(from_client)
-        conn.send(b"I am SERVER<br>")
+        from_client += data
+    conn.send(b"[Server] received data: %a" % len(from_client))
+    print("received data: %a" % len(from_client))
     conn.close()
     print("client disconnected")
