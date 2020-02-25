24d314249f410031110b4c7ecf3a0a5e4e109f44
gamesguru <mathmuncher11@gmail.com>
2020-02-24 20:44:35 -0500

    remove commented test code, upate crypt

--- qc/crypt.py
+++ qc/crypt.py
@@ -1,5 +1,26 @@
 import base64
+import random
+import sys
 from hashlib import sha3_256 as sha3
+
+import pyspx.shake256_128s as sphincs
+
+
+def generate_keypair(seed_words, password):
+
+    print("\nseed_phrase: %s" % " ".join(seed_words))
+    password = "secret"
+    if password:
+        seed_words.append(password)
+
+    # Create bytes-seed
+    random.seed(" ".join(seed_words))
+    r = random.getrandbits(384)
+    seed = r.to_bytes(48, sys.byteorder)
+
+    pk, sk = sphincs.generate_keypair(seed)
+
+    return pk, sk
 
 
 def v1_address(pk, n_id):
